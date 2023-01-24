0833aad470ff3a5753de88bbe4e3653f113d8a49
careyi3 <ian@iancarey.ie>
2023-01-24 16:44:28 +0000

    Test code

---
+++ code/arduino/tests/sensor_test/sensor_test.ino
@@ -0,0 +1,21 @@
+#include <HCSR04.h>
+#include <SimpleKalmanFilter.h>
+
+HCSR04 sensor(5, 6);
+SimpleKalmanFilter filter(0.03, 0.03, 0.01);
+double value;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  double reading = sensor.dist();
+  if (reading > 2 && reading < 18)
+  {
+    Serial.print(reading);
+    Serial.print(",");
+    Serial.println(filter.updateEstimate(reading));
+  }
+  delay(20);
+}
